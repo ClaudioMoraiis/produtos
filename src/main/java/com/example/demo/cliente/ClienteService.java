a025a7f0bc295b1f2debeb9e34bf6063fa6ffd52
Claudio <claudiomoraisbn@gmail.com>
2025-05-22 20:37:19 -0300

    Criado método para formatar os documentos (Adicionar "-./")

--- src/main/java/com/example/demo/cliente/ClienteService.java
+++ src/main/java/com/example/demo/cliente/ClienteService.java
@@ -1,14 +1,10 @@
 package com.example.demo.cliente;
 
-import com.example.demo.enums.TipoDocumentoEnum;
-import com.example.demo.usuario.UsuarioEntity;
 import com.example.demo.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -17,10 +13,14 @@ public class ClienteService {
 
     public ResponseEntity<?> cadastrar(ClienteDTO mClienteDTO){
         try {
-            ClienteEntity mUsuarioEntity = fRepository.findByDocumento(mClienteDTO.getDocumento());
+            ClienteEntity mUsuarioEntity = fRepository.findFirstByDocumentoOrderByIdAsc(Util.formatarDocumento(mClienteDTO.getDocumento()));
             if (mUsuarioEntity != null) {
                 return ResponseEntity.status(HttpStatus.CONFLICT).body("Já existe cliente cadastrado com esse documento");
             }
+
+            String mDocumentoFormatado = mClienteDTO.getDocumento().replaceAll("[./-]", "");
+            if ((mDocumentoFormatado.length() != 11) && (mDocumentoFormatado.length() != 14))
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Insira um documento válido");
 
             fRepository.save(Cliente.preencherVO(mClienteDTO));
             return ResponseEntity.status(HttpStatus.OK).body("Cliente cadastrado com sucesso.");
--- src/main/java/com/example/demo/util/Util.java
+++ src/main/java/com/example/demo/util/Util.java
@@ -6,6 +6,14 @@ public class Util {
     public static TipoDocumentoEnum validarTipoDocumento(String mDocumento){
        mDocumento = mDocumento.replaceAll("[./-]", "");
 
-       return mDocumento.trim().length() == 14 ? TipoDocumentoEnum.CNPJ : TipoDocumentoEnum.CNPJ;
+       return mDocumento.trim().length() == 14 ? TipoDocumentoEnum.CNPJ : TipoDocumentoEnum.CPF;
+    }
+
+    public static String formatarDocumento(String mDocumento){
+        mDocumento = mDocumento.replaceAll("[./-]", "");
+        if (mDocumento.trim().length() == 14)
+            return  mDocumento.replaceAll("(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{2})", "$1.$2.$3/$4-$5");
+        else
+            return mDocumento.replaceAll("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
     }
 }
