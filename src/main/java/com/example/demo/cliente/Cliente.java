a91d5a5f8f4287531c09f08c82c24f6ff3e3bf81
Claudio <claudiomoraisbn@gmail.com>
2025-07-15 20:11:33 -0300

    Criado rota para alterar cliente

--- src/main/java/com/example/demo/cliente/Cliente.java
+++ src/main/java/com/example/demo/cliente/Cliente.java
@@ -26,6 +26,31 @@ public class Cliente {
         mClienteEntity.setCep(mCliente.getCep());
         mClienteEntity.setAtivo(mCliente.getAtivo());
         mClienteEntity.setDataCadastro(LocalDate.now());
+        mClienteEntity.setComplemento(mCliente.getComplemento());
+
+        return mClienteEntity;
+    }
+
+    public static ClienteEntity alterar(ClienteEntity mClienteEntity, ClienteDados mCliente){
+        mClienteEntity.setNome(mCliente.getNome());
+
+        if (Util.validarTipoDocumento(mCliente.getDocumento()) == TipoDocumentoEnum.CNPJ)
+            mClienteEntity.setTipoDocumento(TipoDocumentoEnum.CNPJ);
+        else
+            mClienteEntity.setTipoDocumento(TipoDocumentoEnum.CPF);
+
+        mClienteEntity.setDocumento(Util.formatarDocumento(mCliente.getDocumento()));
+        mClienteEntity.setDataNascimento(mCliente.getDataNascimento());
+        mClienteEntity.setEmail(mCliente.getEmail());
+        mClienteEntity.setTelefone(Util.formatarTelefone(mCliente.getTelefone()));
+        mClienteEntity.setEndereco(mCliente.getEndereco());
+        mClienteEntity.setBairro(mCliente.getBairro());
+        mClienteEntity.setCidade(mCliente.getCidade());
+        mClienteEntity.setEstado(mCliente.getEstado());
+        mClienteEntity.setCep(mCliente.getCep());
+        mClienteEntity.setAtivo(mCliente.getAtivo());
+        mClienteEntity.setDataCadastro(LocalDate.now());
+        mClienteEntity.setComplemento(mCliente.getComplemento());
 
         return mClienteEntity;
     }
